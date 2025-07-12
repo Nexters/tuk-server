2a1269645d12274e53e5840417e8e5eb167238d7
songyi00 <ksl2950@naver.com>
2025-07-13 00:06:44 +0900

    feat: add MySQL and Redis integration (For Dev)

--- tuk-api/build.gradle.kts
+++ tuk-api/build.gradle.kts
@@ -1,11 +1,17 @@
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// mysql
+	implementation("mysql:mysql-connector-java:${properties["mysqlVersion"]}")
+
+	// swagger
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${properties["swaggerVersion"]}")
 }
 
