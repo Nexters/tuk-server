2116a0626d88548981daba287b50e111107d3331
JunRain <sa2313@tukorea.ac.kr>
2025-07-18 07:32:19 +0900

    feat: init tuk-batch

---
+++ tuk-batch/build.gradle.kts
@@ -0,0 +1,35 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // quartz
+    implementation("org.springframework.boot:spring-boot-starter-quartz:${project.properties["quartzVersion"]}")
+
+
+    // mysql
+    runtimeOnly("com.mysql:mysql-connector-j:${properties["mysqlVersion"]}")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:${project.properties["mockitoKotlinVersion"]}")
+    testImplementation("com.ninja-squad:springmockk:${project.properties["springMockkVersion"]}")
+
+    // testcontainers
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("com.redis:testcontainers-redis")
+    testImplementation("org.testcontainers:mysql")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    systemProperty("spring.profiles.active", "test")
+}
---
+++ tuk-batch/src/test/kotlin/com/junrain/tukbatch/TukBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.junrain.tukbatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TukBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
