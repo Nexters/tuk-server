d95f62ff9150d6288621971ae08abef25509f1c5
JunRain <sa2313@tukorea.ac.kr>
2025-07-26 01:34:06 +0900

    feat: init.sql에 추가

--- docker/init.sql
+++ docker/init.sql
@@ -15,3 +15,53 @@ CREATE TABLE IF NOT EXISTS member
     INDEX idx_email (email),
     INDEX idx_deleted_at (deleted_at)
 );
+
+CREATE TABLE IF NOT EXISTS gathering
+(
+    id                   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    gathering_name       VARCHAR(255) NOT NULL,
+    member_id            BIGINT       NOT NULL,
+    first_gathering_date DATE         NOT NULL,
+    last_gathering_date  DATE         NOT NULL,
+    interval_days        BIGINT       NOT NULL,
+    tags                 JSON         NULL,
+    created_at           TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at           TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at           TIMESTAMP    NULL,
+
+    INDEX idx_deleted_at (deleted_at),
+
+    CONSTRAINT fk_gathering_member FOREIGN KEY (member_id) REFERENCES member(id)
+);
+
+CREATE TABLE IF NOT EXISTS gathering_member
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    gathering_id BIGINT      NOT NULL,
+    member_id    BIGINT      NOT NULL,
+    is_host      BIT         NOT NULL,
+    created_at   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at   TIMESTAMP   NULL,
+
+    CONSTRAINT fk_gathering_member_gathering_id FOREIGN KEY (gathering_id) REFERENCES gathering(id),
+    CONSTRAINT fk_gathering_member_member_id FOREIGN KEY (member_id) REFERENCES member(id),
+    INDEX idx_gathering_member (gathering_id, member_id),
+    INDEX idx_deleted_at (deleted_at)
+);
+
+CREATE TABLE IF NOT EXISTS invitation
+(
+    id           BIGINT AUTO_INCREMENT PRIMARY KEY,
+    gathering_id BIGINT       NOT NULL,
+    member_id    BIGINT       NOT NULL,
+    purpose      VARCHAR(255) NOT NULL,
+    created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at   TIMESTAMP    NULL,
+
+    CONSTRAINT fk_invitation_gathering_id FOREIGN KEY (gathering_id) REFERENCES gathering(id),
+    CONSTRAINT fk_invitation_member_id FOREIGN KEY (member_id) REFERENCES member(id),
+    INDEX idx_gathering_member (gathering_id, member_id),
+    INDEX idx_deleted_at (deleted_at)
+);
--- tuk-api/src/main/kotlin/nexters/tuk/domain/BaseEntity.kt
+++ tuk-api/src/main/kotlin/nexters/tuk/domain/BaseEntity.kt
@@ -1,14 +1,12 @@
 package nexters.tuk.domain
 
 import jakarta.persistence.*
-import org.hibernate.annotations.SoftDelete
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @EntityListeners(AuditingEntityListener::class)
-@SoftDelete
 @MappedSuperclass
 abstract class BaseEntity {
     @Id
