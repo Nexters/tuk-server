7d2577296eb9424c62c8254947054533eda29045
Songyi Kim <ksl2950@naver.com>
2025-08-05 21:14:13 +0900

    feat: 푸시 API 개발 (모임 푸시, 초대장 푸시)

--- tuk-api/src/main/kotlin/nexters/tuk/application/push/dto/request/PushCommand.kt
+++ tuk-api/src/main/kotlin/nexters/tuk/application/push/dto/request/PushCommand.kt
@@ -17,19 +17,16 @@ class PushCommand {
     )
     sealed interface Push {
         val pushType: PushType
-        val message: MessagePayload
 
         @Schema(title = "모임 정기 푸시 (GATHERING_NOTIFICATION)")
         data class GatheringNotification(
             val recipients: List<PushRecipient>,
             override val pushType: PushType = PushType.GATHERING_NOTIFICATION,
-            override val message: MessagePayload,
         ) : Push
 
         @Schema(title = "초대장 푸시 (PROPOSAL)")
         data class Proposal(
             override val pushType: PushType = PushType.PROPOSAL,
-            override val message: MessagePayload,
             val gatheringId: Long,
         ) : Push
     }
