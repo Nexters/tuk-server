39825b5319e2f60c62fc47b9bb3f772846f90080
JunRain <sa2313@tukorea.ac.kr>
2025-08-06 20:16:46 +0900

    fix: swagger 수

--- tuk-api/src/main/kotlin/nexters/tuk/ui/purpose/PurposeSpec.kt
+++ tuk-api/src/main/kotlin/nexters/tuk/ui/purpose/PurposeSpec.kt
@@ -1,15 +1,12 @@
 package nexters.tuk.ui.purpose
 
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import nexters.tuk.application.purpose.dto.response.PurposeResponse
-import nexters.tuk.config.SwaggerConfig
 import nexters.tuk.contract.ApiResponse
 
 interface PurposeSpec {
     @Operation(
-        summary = "여행 목적 전체 조회",
-        security = [SecurityRequirement(name = SwaggerConfig.SECURITY_SCHEME_NAME)]
+        summary = "만남 목적 전체 조회",
     )
     fun getAllPurposes(): ApiResponse<PurposeResponse.Purposes>
 }
