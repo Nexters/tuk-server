fb797644b0e0381d3b3750e902984427b50effa4
songyi00 <ksl2950@naver.com>
2025-08-06 21:59:05 +0900

    feat: 회원 탈퇴

--- tuk-api/src/main/kotlin/nexters/tuk/domain/member/MemberRepository.kt
+++ tuk-api/src/main/kotlin/nexters/tuk/domain/member/MemberRepository.kt
@@ -2,9 +2,13 @@ package nexters.tuk.domain.member
 
 import nexters.tuk.application.member.SocialType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface MemberRepository : JpaRepository<Member, Long> {
-    fun findByEmail(email: String): Member?
-
     fun findBySocialTypeAndSocialId(socialType: SocialType, socialId: String): Member?
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("delete from Member where id =:memberId")
+    fun leave(memberId: Long)
 }
