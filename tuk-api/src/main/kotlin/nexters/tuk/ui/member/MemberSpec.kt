4c35e8476282768c6236a598c86bf79f0dc01ca3
JunRain <sa2313@tukorea.ac.kr>
2025-08-08 21:14:28 +0900

    feat: 사용자 프로필 조회 API

--- tuk-api/src/main/kotlin/nexters/tuk/ui/member/MemberSpec.kt
+++ tuk-api/src/main/kotlin/nexters/tuk/ui/member/MemberSpec.kt
@@ -1,9 +1,13 @@
 package nexters.tuk.ui.member
 
 import io.swagger.v3.oas.annotations.Operation
+import nexters.tuk.application.member.dto.response.MemberResponse
 import nexters.tuk.contract.ApiResponse
 
 interface MemberSpec {
     @Operation(summary = "회원 탈퇴")
-    fun deleteMember(memberId: Long) : ApiResponse<Unit>
+    fun deleteMember(memberId: Long): ApiResponse<Unit>
+
+    @Operation(summary = "회원 조회")
+    fun getMember(memberId: Long): ApiResponse<MemberResponse.Profile>
 }
