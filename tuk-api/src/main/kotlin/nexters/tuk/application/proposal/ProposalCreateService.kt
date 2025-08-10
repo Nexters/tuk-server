81fb690bba32afbe8cdaed69cd46af9cf6c3c202
songyi00 <ksl2950@naver.com>
2025-08-10 20:36:14 +0900

    feat: 초대장 발행시 초대장 푸시 연동

--- tuk-api/src/main/kotlin/nexters/tuk/application/proposal/ProposalCreateService.kt
+++ tuk-api/src/main/kotlin/nexters/tuk/application/proposal/ProposalCreateService.kt
@@ -3,6 +3,9 @@ package nexters.tuk.application.proposal
 import nexters.tuk.application.gathering.GatheringMemberService
 import nexters.tuk.application.proposal.dto.request.ProposalCommand
 import nexters.tuk.application.proposal.dto.response.ProposalResponse
+import nexters.tuk.application.push.PushService
+import nexters.tuk.application.push.dto.request.PushCommand
+import nexters.tuk.contract.push.PushType
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -11,6 +14,7 @@ class ProposalCreateService(
     private val proposalService: ProposalService,
     private val proposalMemberService: ProposalMemberService,
     private val gatheringMemberService: GatheringMemberService,
+    private val pushService: PushService,
 ) {
     @Transactional
     fun propose(command: ProposalCommand.Propose): ProposalResponse.Propose {
@@ -26,6 +30,12 @@ class ProposalCreateService(
             memberIds = gatheringMembers
         )
 
+        pushService.sendPush(
+            PushCommand.Push.Proposal(
+                pushType = PushType.PROPOSAL,
+                gatheringId = command.gatheringId
+            )
+        )
         return ProposalResponse.Propose(proposalId = proposal.proposalId)
     }
 }
