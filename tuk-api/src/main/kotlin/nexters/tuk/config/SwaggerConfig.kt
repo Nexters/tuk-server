38b5ac4be6a966d55a42d7df203cc8a6947571f4
JunRain <sa2313@tukorea.ac.kr>
2025-08-11 01:33:34 +0900

    fix: swagger-ui 수정

--- tuk-api/src/main/kotlin/nexters/tuk/config/SwaggerConfig.kt
+++ tuk-api/src/main/kotlin/nexters/tuk/config/SwaggerConfig.kt
@@ -1,10 +1,13 @@
 package nexters.tuk.config
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.context.annotation.Configuration
 
 
+@OpenAPIDefinition(servers = [Server(url = "/", description = "Default Server URL")])
 @SecurityScheme(
     name = "Authorization",
     type = SecuritySchemeType.HTTP,
