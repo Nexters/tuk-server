2086cd61be2fb2785820900737d7b7982e06d751
JunRain <sa2313@tukorea.ac.kr>
2025-08-12 23:42:38 +0900

    feat: CORS 설정 추가

--- tuk-api/src/main/kotlin/nexters/tuk/config/WebMvcConfig.kt
+++ tuk-api/src/main/kotlin/nexters/tuk/config/WebMvcConfig.kt
@@ -3,6 +3,7 @@ package nexters.tuk.config
 import nexters.tuk.ui.resolver.AccessTokenResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -11,5 +12,13 @@ class WebMvcConfig(
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(accessTokenResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "https://tuk.kr")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
     }
 }
