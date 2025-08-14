4993764b5423294ef9ffbe93a4ed360a22be791f
JunRain <sa2313@tukorea.ac.kr>
2025-08-14 16:23:56 +0900

    fix: Swagger 수정

--- tuk-api/src/main/kotlin/nexters/tuk/ui/device/DeviceSpec.kt
+++ tuk-api/src/main/kotlin/nexters/tuk/ui/device/DeviceSpec.kt
@@ -1,15 +1,18 @@
 package nexters.tuk.ui.device
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import nexters.tuk.application.device.dto.request.DeviceCommand
 import nexters.tuk.application.device.dto.response.DeviceResponse
+import nexters.tuk.config.SwaggerConfig
 import nexters.tuk.contract.ApiResponse
 import org.springframework.web.bind.annotation.RequestBody
 
 interface DeviceSpec {
     @Operation(
         summary = "디바이스 토큰 업데이트",
-        description = "사용자의 디바이스 토큰을 업데이트합니다. 신규 디바이스인 경우 등록하고, 기존 디바이스인 경우 토큰을 업데이트합니다."
+        description = "사용자의 디바이스 토큰을 업데이트합니다. 신규 디바이스인 경우 등록하고, 기존 디바이스인 경우 토큰을 업데이트합니다.",
+        security = [SecurityRequirement(name = SwaggerConfig.SECURITY_SCHEME_NAME)]
     )
     fun updateDeviceToken(
         memberId: Long,
